37133fa892bfdc0362158dbf111e90a84b671f84
Luiz Cezer <lccezinha@gmail.com>
2021-10-02 16:03:21 -0300

    Add credo fixes

--- lib/nfl_rushing/mailer.ex
+++ lib/nfl_rushing/mailer.ex
@@ -1,3 +1,4 @@
 defmodule NflRushing.Mailer do
+  @moduledoc false
   use Swoosh.Mailer, otp_app: :nfl_rushing
 end
--- lib/nfl_rushing_web/telemetry.ex
+++ lib/nfl_rushing_web/telemetry.ex
@@ -2,6 +2,8 @@ defmodule NflRushingWeb.Telemetry do
   use Supervisor
   import Telemetry.Metrics
 
+  @moduledoc false
+
   def start_link(arg) do
     Supervisor.start_link(__MODULE__, arg, name: __MODULE__)
   end
