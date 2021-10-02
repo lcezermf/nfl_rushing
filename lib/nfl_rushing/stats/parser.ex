68cde6a7280ba0d29b39bae1f57f490c97419e12
Luiz Cezer <lccezinha@gmail.com>
2021-10-02 17:45:17 -0300

    Map each JSON row into a map equals player fields

--- lib/nfl_rushing/stats/parser.ex
+++ lib/nfl_rushing/stats/parser.ex
@@ -3,13 +3,32 @@ defmodule NflRushing.Stats.Parser do
   NflRushing.Stats.Parser is a module to parse information about a player by reading data from JSON.
   """
 
-
   @spec parse_players(binary()) :: {:ok, [map()]} | {:error, atom}
   def parse_players(filename) do
     with {:ok, body} <- File.read(filename),
-         {:ok, _parsed_json} <- Jason.decode(body)
-    do
-      {:ok, %{}}
+         {:ok, parsed_json} <- Jason.decode(body) do
+      {:ok, Enum.map(parsed_json, fn player -> parse_player(player) end)}
     end
+  end
+
+  @spec parse_player(map()) :: map()
+  defp parse_player(player) do
+    %{
+      name: player["Player"],
+      team: player["Team"],
+      position: player["Pos"],
+      rushing_attempts_per_game_average: player["Att/G"],
+      rushing_attempts_total: player["Att"],
+      rushing_yards_total: player["Yds"],
+      rushing_yards_per_attempt_average: player["Avg"],
+      rushing_yards_per_game: player["Yds/G"],
+      rushing_touchdowns_total: player["TD"],
+      rushing_longest_touchdown: player["Lng"],
+      rushing_first_downs_total: player["1st"],
+      rushing_first_downs_percentage: player["1st%"],
+      rushing_20_yards_more: player["20+"],
+      rushing_40_yards_more: player["40+"],
+      fumbles: player["FUM"]
+    }
   end
 end
---
+++ test/nfl_rushing/stats/parser_test.exs
@@ -0,0 +1,52 @@
+defmodule NflRushing.Stats.ParserTest do
+  use NflRushing.DataCase
+
+  alias NflRushing.Stats.Parser
+
+  describe "parse_players/1" do
+    test "with invalid file path must raise error" do
+      assert {:error, :enoent} == Parser.parse_players("invalid_file_path.json")
+    end
+
+    test "must map columns from JSON and map to Player schema" do
+      expected_players = [
+        %{
+          fumbles: 0,
+          name: "Joe Banyard",
+          position: "RB",
+          rushing_20_yards_more: 0,
+          rushing_40_yards_more: 0,
+          rushing_attempts_per_game_average: 2,
+          rushing_attempts_total: 2,
+          rushing_first_downs_percentage: 0,
+          rushing_first_downs_total: 0,
+          rushing_longest_touchdown: "7",
+          rushing_touchdowns_total: 0,
+          rushing_yards_per_attempt_average: 3.5,
+          rushing_yards_per_game: 7,
+          rushing_yards_total: 7,
+          team: "JAX"
+        },
+        %{
+          fumbles: 2,
+          name: "Mark Ingram",
+          position: "RB",
+          rushing_20_yards_more: 4,
+          rushing_40_yards_more: 2,
+          rushing_attempts_per_game_average: 12.8,
+          rushing_attempts_total: 205,
+          rushing_first_downs_percentage: 23.9,
+          rushing_first_downs_total: 49,
+          rushing_longest_touchdown: "75T",
+          rushing_touchdowns_total: 6,
+          rushing_yards_per_attempt_average: 5.1,
+          rushing_yards_per_game: 65.2,
+          rushing_yards_total: "1,043",
+          team: "NO"
+        }
+      ]
+
+      assert {:ok, expected_players} == Parser.parse_players("test/support/fixtures/rushing.json")
+    end
+  end
+end
