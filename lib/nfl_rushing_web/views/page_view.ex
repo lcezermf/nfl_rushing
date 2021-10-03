d380b05ed47de12ab39e18f5e53115f9e966b212
Luiz Cezer <lccezinha@gmail.com>
2021-10-03 18:02:10 -0300

    Create get_order_direction function to allow order column asc or desc

--- lib/nfl_rushing_web/views/page_view.ex
+++ lib/nfl_rushing_web/views/page_view.ex
@@ -1,3 +1,7 @@
 defmodule NflRushingWeb.PageView do
   use NflRushingWeb, :view
+
+  def get_order_direction(""), do: "asc"
+  def get_order_direction("asc"), do: "desc"
+  def get_order_direction("desc"), do: "asc"
 end
--- test/nfl_rushing_web/views/page_view_test.exs
+++ test/nfl_rushing_web/views/page_view_test.exs
@@ -1,3 +1,19 @@
 defmodule NflRushingWeb.PageViewTest do
   use NflRushingWeb.ConnCase, async: true
+
+  alias NflRushingWeb.PageView
+
+  describe "get_order_direction/1" do
+    test "when no value is given must return asc" do
+      assert PageView.get_order_direction("") == "asc"
+    end
+
+    test "when asc is given as param must return desc" do
+      assert PageView.get_order_direction("asc") == "desc"
+    end
+
+    test "when desc is given as param must return asc" do
+      assert PageView.get_order_direction("desc") == "asc"
+    end
+  end
 end
