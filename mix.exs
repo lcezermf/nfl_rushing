b54c390ad8f39936f24ce36a94d7d1f2270643bb
Luiz Cezer <lccezinha@gmail.com>
2021-10-04 22:50:33 -0300

    Add dyalizer

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule NflRushing.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
