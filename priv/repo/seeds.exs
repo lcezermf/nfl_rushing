649b090eaed07ba73b626b3012c63f4450003d74
Luiz Cezer <lccezinha@gmail.com>
2021-10-06 16:39:54 -0300

    Change batch size to import data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -13,7 +13,7 @@
 {:ok, result} = NflRushing.Stats.Parser.parse_players("priv/rushing.json")
 
 # For the inital file with 326 records will not be used, but will be applied with I need to load a larger file.
-batch_size = 1000
+batch_size = 50
 
 Enum.chunk_every(result, batch_size)
 |> Enum.each(fn batch -> NflRushing.Repo.insert_all(NflRushing.Stats.Player, batch) end)
